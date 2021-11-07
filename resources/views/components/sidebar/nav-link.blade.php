7048dcefe9844172918fa61a6ff5130b06bdfce8
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-11-07 21:01:56 +0530

    created some components

--- resources/views/components/sidebar/nav-link.blade.php
+++ resources/views/components/sidebar/nav-link.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'h-16 px-6 flex flex justify-center items-center w-full focus:text-orange-500 hover:text-blue-500']) }}>
+<a {{ $attributes->merge(['class' => 'h-16 px-6 flex flex justify-center items-center w-full hover:text-blue-500']) }}>
 
    {{ $slot }}
 
