1c60d85062fd1b820818228cc1903847ba641668
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-11-18 23:04:56 +0530

    created Setting Model and Migration File

---
+++ app/Models/Setting.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    public static function timeFormats()
+    {
+        return [
+            'g:i A',
+            'h:i A',
+            'H:i',
+        ];
+    }
+
+    public static function dateFormats()
+    {
+        return [
+            'Y-m-d',
+            'D M d, Y',
+            'M j, Y',
+            'd M, Y',
+            'm/d/Y',
+            'n/d/y',
+            'd/m/Y',
+            'd.m.Y',
+            'Y.m.d',
+        ];
+    }
+
+    public static function timezones()
+    {
+        return timezone_identifiers_list();
+    }
+
+    /**
+     * Get the app settings
+     */
+    public static function getSettings()
+    {
+        try {
+            return self::firstOrNew();
+        } catch (\Throwable $throwable) {
+            return null;
+        }
+    }
+}
