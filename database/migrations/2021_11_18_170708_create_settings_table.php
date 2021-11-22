d8fc240e73603fdbb23800f6d49bb971669eb49a
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-11-22 15:43:18 +0530

    Modified Setting Migration File

--- database/migrations/2021_11_18_170708_create_settings_table.php
+++ database/migrations/2021_11_18_170708_create_settings_table.php
@@ -19,7 +19,7 @@ class CreateSettingsTable extends Migration
             $table->string('color')->default('light');
             $table->string('logo_path')->nullable()->default(null);
             $table->char('favicon_path')->nullable()->default(null);
-            $table->string('loginBackground_path')->nullable()->default('img/default-bg.jpg');
+            $table->string('loginBackground_path')->nullable();
             $table->string('timezone',100)->nullable()->default(config('app.timezone'));
             $table->string('date_format')->default('Y-m-d');
             $table->string('time_format')->default('h:i A');
