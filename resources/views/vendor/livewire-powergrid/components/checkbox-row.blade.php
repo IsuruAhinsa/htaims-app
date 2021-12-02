a195ed804d885621f3032bdaee6c879750aeb7d7
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-02 19:51:19 +0530

    Modified Dark Mode Classes

--- resources/views/vendor/livewire-powergrid/components/checkbox-row.blade.php
+++ resources/views/vendor/livewire-powergrid/components/checkbox-row.blade.php
@@ -7,7 +7,7 @@
     <td class="{{ $theme->thClass }}" style="{{ $theme->thStyle }}">
         <div class="{{ $theme->divClass }}">
             <label class="{{ $theme->labelClass }}">
-                <input class="{{ $theme->inputClass }} form-checkbox rounded border-gray-300 dark:border-transparent text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring dark:focus:ring-0 focus:ring-indigo-200 focus:ring-opacity-50 bg-dark-third" type="checkbox" wire:model.defer="checkboxValues"
+                <input class="{{ $theme->inputClass }} form-checkbox rounded border-gray-300 dark:border-transparent text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring dark:focus:ring-0 focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-dark-third" type="checkbox" wire:model.defer="checkboxValues"
                        value="{{ $attribute }}">
             </label>
         </div>
