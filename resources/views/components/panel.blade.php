39c8e6cee40ac3d00011f59e406f1ca276060a9f
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-07 14:59:06 +0530

    Created Panel Component

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,101 @@
+@props(['id'])
+
+@php
+  $id = $id ?? md5($attributes->wire('model'));
+@endphp
+
+<div
+    class="fixed inset-0 overflow-hidden"
+    aria-labelledby="slide-over-title"
+    role="dialog"
+    aria-modal="true"
+    x-data="{
+        show: @entangle($attributes->wire('model')).defer,
+        focusables() {
+            // All focusable element types...
+            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
+
+            return [...$el.querySelectorAll(selector)]
+                // All non-disabled elements...
+                .filter(el => ! el.hasAttribute('disabled'))
+        },
+        firstFocusable() { return this.focusables()[0] },
+        lastFocusable() { return this.focusables().slice(-1)[0] },
+        nextFocusable() { return this.focusables()[this.nextFocusableIndex()] || this.firstFocusable() },
+        prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable() },
+        nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
+        prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
+    }"
+    x-init="$watch('show', value => {
+        if (value) {
+            document.body.classList.add('overflow-y-hidden');
+            {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
+        } else {
+            document.body.classList.remove('overflow-y-hidden');
+        }
+    })"
+    x-on:close.stop="show = false"
+    x-on:keydown.escape.window="show = false"
+    x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
+    x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
+    x-show="show"
+    id="{{ $id }}"
+    style="display: none;"
+>
+
+    <div class="absolute inset-0 overflow-hidden">
+
+        <div
+            x-show="show" class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            aria-hidden="true"
+            x-on:click="show = false"
+            x-transition:enter="ease-in-out duration-500"
+            x-transition:enter-start="opacity-0"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="ease-in-out duration-500"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+        ></div>
+
+        <div class="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+
+            <div x-show="show" class="relative w-screen max-w-md"
+                x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
+                x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
+                x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700"
+                x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+
+                <div x-show="show" class="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4"
+                    x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0"
+                    x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500"
+                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+                    <button type="button"
+                            x-on:click="show = false"
+                        class="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
+                        <span class="sr-only">Close panel</span>
+
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                    <div class="px-4 sm:px-6">
+                        <h2 class="flex items-center text-lg font-medium text-gray-900" id="slide-over-title">
+                            {{ $title }}
+                        </h2>
+                    </div>
+                    <div class="mt-6 relative flex-1 px-4 sm:px-6">
+                        <div class="absolute inset-0 px-4 sm:px-6">
+                            {{ $content }}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
