dcbd05a3b629d3413db2c70453a1948dabe405a1
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-13 09:38:10 +0530

    Added close button to modal

--- resources/views/vendor/jetstream/components/dialog-modal.blade.php
+++ resources/views/vendor/jetstream/components/dialog-modal.blade.php
@@ -2,8 +2,15 @@
 
 <x-jet-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
     <div class="px-6 py-4">
-        <div class="flex items-center text-lg dark:text-white">
-            {{ $title }}
+        <div class="flex items-center justify-between text-lg dark:text-white">
+
+            <div class="flex items-center">
+                {{ $title }}
+            </div>
+
+            <svg x-on:click.stop="show = false" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-gray-600 hover:cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
         </div>
 
         <div class="mt-4 dark:text-dark-typography">
