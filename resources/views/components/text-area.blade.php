be87ba515b19938bd27d5042cab778e9795de705
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-17 12:59:37 +0530

    Modified Textarea component

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<textarea cols="10" rows="5" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'form-textarea border-gray-300 dark:bg-dark-third dark:text-dark-typography dark:border-transparent focus:border-indigo-300 dark:focus:border-indigo-500 focus:ring dark:focus:ring-0 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) }}></textarea>
+<textarea cols="10" style="height: 121px" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'form-textarea border-gray-300 dark:bg-dark-third dark:text-dark-typography dark:border-transparent focus:border-indigo-300 dark:focus:border-indigo-500 focus:ring dark:focus:ring-0 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm resize-none']) }}></textarea>
