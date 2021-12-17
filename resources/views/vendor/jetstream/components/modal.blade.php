fe2b4e0c5dd1811aa23dcd017bcd97e3355b880f
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-17 12:59:19 +0530

    Modified JetStream modal

--- resources/views/vendor/jetstream/components/modal.blade.php
+++ resources/views/vendor/jetstream/components/modal.blade.php
@@ -9,6 +9,11 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
+    '3xl' => 'sm:max-w-3xl',
+    '4xl' => 'sm:max-w-4xl',
+    '5xl' => 'sm:max-w-5xl',
+    '6xl' => 'sm:max-w-6xl',
+    '7xl' => 'sm:max-w-7xl',
 ][$maxWidth ?? '2xl'];
 @endphp
 
@@ -62,11 +67,11 @@ $maxWidth = [
             x-transition:leave-end="opacity-0"
         ></div>
 
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <span class="hidden sm:inline-block sm:align-middle md:h-full lg:h-screen" aria-hidden="true">&#8203;</span>
 
         <div
             x-show="show"
-            class="inline-block align-bottom sm:align-middle bg-white dark:bg-dark-secondary rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:w-full {{ $maxWidth }} sm:mx-auto"
+            class="inline-block sm:align-middle bg-white dark:bg-dark-secondary rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:w-full {{ $maxWidth }} sm:mx-auto"
             x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
