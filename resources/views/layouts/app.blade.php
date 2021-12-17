a755099b615f47534c76244ca8c3c6c762a2d068
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-12-17 12:55:39 +0530

    changed main layout due to powergrid errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,35 +37,31 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-100 dark:bg-dark-primary min-h-screen">
 
         <x-notifications z-index="z-50" />
 
         <x-jet-banner />
 
-        <div class="flex min-h-full bg-gray-100 dark:bg-dark-primary">
+        @livewire('partials.sidebar')
 
-            @livewire('partials.sidebar')
+        <div class="ml-20">
 
-            <div class="flex-1">
+            @livewire('navigation-menu')
 
-                @livewire('navigation-menu')
-
-                <!-- Page Heading -->
-                {{--@if (isset($header))
+            <!-- Page Heading -->
+            {{--@if (isset($header))
                     <header class="bg-white shadow">
                         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                             {{ $header }}
                         </div>
                     </header>
-                @endif--}}
+            @endif--}}
 
-                <!-- Page Content -->
-                <main>
-                    {{ $slot }}
-                </main>
-
-            </div>
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
 
         </div>
 
